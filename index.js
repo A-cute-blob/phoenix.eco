77bfcc9afeb8fef927d8f56e09657f08be3ce4a4
Tatsumaki <77403361+tatsumaki-coderr@users.noreply.github.com>
2021-02-09 10:53:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let db = require('mongoose')
 let eco = require('./schemas/eco')
-
+let currencyModel = eco;
 db.set('useFindAndModify', false)
 /**
 *
@@ -52,7 +52,7 @@ if(amount < 0) throw new TypeError("The amount cannot be under 0")
         let user = await eco.findOne({ userId: id});
 
         if (!user) {
-            const newData = new currencyModel({
+            const newData = new eco({
                 userId: id,
                 bankSpace: 1000,
                 coinsInBank: 0,
@@ -87,7 +87,7 @@ if(amount < 0) throw new TypeError("The amount cannot be under 0")
         let user = await eco.findOne({ userId: userId });
 
         if (!user) {
-            const newData = new currencyModel({
+            const newData = new eco({
                 userId: userId,
  
                 bankSpace: 1000,
@@ -116,6 +116,97 @@ if(amount < 0) throw new TypeError("The amount cannot be under 0")
         .catch(err => console.log(err));
 
         return amount;
+    }
+      
+        /**
+     * 
+     * @param {string} userId - A discord user ID.
+     * @param {string} guildId - A discord guild ID.
+     * @param {string} amount - Amount of bank space to give.
+     */
+    
+      static async addbankspace(userId, amount){
+            if(!userId) throw new TypeError('Please provide a user id')
+            if(!amount) throw new Error('Please give me a amount for adding bankspace')
+            if(isNaN(amount)) throw new Error('The amount must be a number')
+            if(amount > 0) throw new Error('The ammount cannot be under 0')
+                    let user = await eco.findOne({ userId: userId });
+
+        if (!user) {
+            let newData = new eco({
+                userId: userId,
+                bankSpace: 1000 + parseInt(amount),
+                coinsInBank: 0,
+                coinsInWallet: 0
+            });
+
+            await newData.save()
+            .catch(err => console.log(err));
+
+            return amount;
+        }
+
+        user.bankSpace += parseInt(amount);
+
+        await user.save()
+        .catch(err => console.log(err));
+
+        return amount;
+    
+      }
+          /**
+     * 
+     * @param {string} userId - A discord user ID.
+     * @param {string} guildId - A discord guild ID.
+     */
+
+    static async newUser(userId) {
+        if (!userId) throw new TypeError("Please provide a user ID.");
+
+        let user = await eco.findOne({ userId: userId, guildId: guildId });
+        if (user) return false;
+
+        let newData = new eco({
+            userId: userId,
+            bankSpace: 1000,
+            coinsInBank: 0,
+            coinsInWallet: 0
+        });
+
+        await newData.save()
+        .catch(err => console.log(err));
+    }
+        /**
+     * 
+     * @param {string} userId - A discord user ID.
+     * @param {string} guildId - A discord guild ID.
+     */
+
+    static async deleteUser(userId) {
+        if (!userId) throw new TypeError("Please provide a user ID.");
+       
+        let user = await currencyModel.findOne({ userId: userId });
+        if (!user) return false;
+
+        await currencyModel.findOneAndRemove({ userId: userId });
+
+        await user.save()
+        .catch(err => console.log(err));
+    }
+      
+    /**
+     * 
+     * @param {string} guildId - A discord guild ID.
+     * @param {number} amount - The amount of users to show.
+     */
+
+    static async generateLeaderboard( amount) {
+        if (!amount) throw new TypeError("Please provide the amount of users to show.");
+        if (isNaN(amount)) throw new TypeError("Amount must be a number");
+
+        let users = await currencyModel.find({}).sort([['coinsInWallet', 'descending']]).exec();
+
+        return users.slice(0, amount);
     }
 }
 module.exports = {
